a3de01e3000ca61d455ba2ee1f254c125edc29e6
Violet Lauro <vinniehat@gmail.com>
2023-05-03 21:30:46 -0400

    Added InitializeApplicationDBContextService

--- Spark.NET.Shared.Infrastructure/Services/ApplicationDbContext/InitializeApplicationDbContextService.cs
+++ Spark.NET.Shared.Infrastructure/Services/ApplicationDbContext/InitializeApplicationDbContextService.cs
@@ -1,6 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace Spark.NET.Infrastructure.Services.ApplicationDbContext;
 
-public class InitializeApplicationDbContextService
+public static class InitializeApplicationDbContextService
 {
-    
+    public static void RegisterService(IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        services.AddDbContext<Contexts.ApplicationDbContext>(options =>
+            options.UseNpgsql(connectionString));
+        /*
+         * If using SQL Server, replace use the following line instead:
+         * - options.UseSqlServer(connectionString);
+         * If using MySQL/MariaDB, replace use the following line instead:
+         * - options.UseMySQL(connectionString, serverVersion: ServerVersion.AutoDetect(connectionString)));
+         * If using SQLite, replace use the following line instead:
+         * - options.UseSqlite(connectionString);
+         * If using PostgreSQL, replace use the following line instead:
+         * - options.UseNpgsql(connectionString);
+        */
+    }
 }
