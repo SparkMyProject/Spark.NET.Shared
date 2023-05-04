29a263f4db1f2a9ee0fbb5223dbaf75c21aaf144
Violet Lauro <vinniehat@gmail.com>
2023-05-03 22:29:07 -0400

    Replaced User with ApplicationUser

---
+++ Spark.NET.Shared.Entities/Models/User/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace Spark.NET.Shared.Entities.Models.User;
+
+public class ApplicationUser : IdentityUser
+{
+    [Required] public DateTime CreatedAt = DateTime.Now;
+
+    [DisplayName("First Name")] public string FirstName { get; set; }
+    [DisplayName("Last Name")] public string LastName { get; set; }
+    [DisplayName("Date of Birth")] public DateOnly DateOfBirth { get; set; }
+
+    public string Address { get; set; }
+}
