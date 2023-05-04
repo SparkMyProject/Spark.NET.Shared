8e7e273899a31f9f159a54fc1d8fed3fd8765a7d
Violet Lauro <vinniehat@gmail.com>
2023-05-03 22:29:27 -0400

    Made RegisterReqModel

---
+++ Spark.NET.Shared.Entities/DTOs/API/Request/Authenticate/RegisterReqModel.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Spark.NET.Shared.Entities.DTOs.API.Request.Authenticate;
+
+public class RegisterReqModel
+{
+    [Required(ErrorMessage = "An Email is required.")] public string Email { get; set; } = "not@required.com";
+    [Required(ErrorMessage = "A UserName is required.")] public string? UserName { get; set; }
+    [Required(ErrorMessage = "A Password is required.")] public string? Password { get; set; }
+    [Required(ErrorMessage = "A FirstName is required.")] public string? FirstName { get; set; }
+    [Required(ErrorMessage = "A Lastname is required.")] public string? LastName { get; set; }
+    public DateOnly DateOfBirth { get; set; }
+    public string Address { get; set; } = "";
+}
