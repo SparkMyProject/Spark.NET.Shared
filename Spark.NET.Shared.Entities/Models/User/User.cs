1e6350caa9522b7187dee31d04e827366d7fccb6
Violet Lauro <vinniehat@gmail.com>
2023-05-03 22:42:20 -0400

    Removed IdentityUser Inheritance from ApplicationUser(now User)

---
+++ Spark.NET.Shared.Entities/Models/User/User.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace Spark.NET.Shared.Entities.Models.User;
+
+public class User
+{
+    [Required] public DateTime CreatedAt = DateTime.Now;
+
+    [Required(ErrorMessage = "An Email is required.")]
+    public string Email { get; set; } = "not@required.com";
+
+    [Required(ErrorMessage = "A UserName is required.")]
+    public string? UserName { get; set; }
+
+    [Required(ErrorMessage = "A Password is required.")]
+    public string? Password { get; set; }
+
+    [Required(ErrorMessage = "A FirstName is required.")]
+    [DisplayName("FirstName")]
+    public string? FirstName { get; set; }
+
+    [Required(ErrorMessage = "A Lastname is required.")]
+    [DisplayName("LastName")]
+    public string? LastName { get; set; }
+
+    [DisplayName("Date of Birth")] public DateOnly DateOfBirth { get; set; }
+
+    public string Address { get; set; }
+}
