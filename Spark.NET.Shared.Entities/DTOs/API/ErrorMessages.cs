b37078a1d62350a969b88dcfca7150a8ab33c81e
Violet Lauro <violetsnow08@gmail.com>
2023-11-25 22:53:28 -0500

    Created ApiErrorMessages model

---
+++ Spark.NET.Shared.Entities/DTOs/API/ErrorMessages.cs
@@ -0,0 +1,9 @@
+namespace Spark.NET.Shared.Entities.DTOs.API;
+
+public static class ApiErrorMessages
+{ 
+    public static readonly string LoginIncorrect = "UserName or Password is incorrect.";
+    public static readonly string RegisterInvalid = "UserName or Password does not meet the criteria.";
+    public static readonly string GenericError = "An error has occurred.";
+
+}
